5159dcc0a62e9a46e90ca14c49cfa7ac17621aaa
nischal basuti <nischalbasuti1@gmail.com>
2019-02-21 17:46:39 +0700

    Added command line argument for shape

--- train.py
+++ train.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-from patched_cnn import _train
 
-_train(size=32)
+import argparse
+from patched_cnn import _train
+  
+parser = argparse.ArgumentParser()
+parser.add_argument("--shape", default=32, help="shape of input")
+args = parser.parse_args()
+
+shape = int(args.shape)
+
+_train(size=shape)
