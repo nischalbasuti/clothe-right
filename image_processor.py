d340c98dc6c59388b49479346e81592c502407ce
nischal basuti <nischalbasuti1@gmail.com>
2019-02-21 17:47:21 +0700

    using yolo to get bounding box of person

--- image_processor.py
+++ image_processor.py
@@ -12,7 +12,10 @@ import copy
 import pymeanshift as pms
 
 class ImageProcessed(object):
-    """docstring for ImageProcessed"""
+    """
+    A convenient class to manipulate an image and it's annotation.
+    Uses reference code from https://www.arunponnusamy.com/yolo-object-detection-opencv-python.html for YOLO.
+    """
 
     def __init__(self, image, annotation):
         """
@@ -152,6 +155,136 @@ class ImageProcessed(object):
 
     def get_segment_images(self, shape=(32, 32)):
         return [ cv2.resize(segment["image"], shape)  for segment in self.segments ]
-#################################################################################
 
+    def get_person_bboxes(self, yolo_weights="./yolo_files/yolov3.weights",
+                         yolo_config="./yolo_files/yolov3.cfg",
+                         yolo_classes="./yolo_files/coco.names",
+                         display = False, **kwargs):
+        """
+        Method to get bounding boxes of self.image.
+        
+        reference code from:
+        https://www.arunponnusamy.com/yolo-object-detection-opencv-python.html
+        """
+        Width = self.image.shape[1]
+        Height = self.image.shape[0]
+        scale = 0.00392
+
+        # read class names from text file
+        self.classes = None
+        with open(yolo_classes, 'r') as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
+        # generate different colors for different classes 
+        self.COLORS = np.random.uniform(0, 255, size=(len(self.classes), 3))
+
+        # read pre-trained model and config file
+        net = cv2.dnn.readNet(yolo_weights, yolo_config)
+
+        # create input blob 
+        blob = cv2.dnn.blobFromImage(self.image, scale, (416,416), (0,0,0), True, crop=False)
+
+        # set input blob for the network
+        net.setInput(blob)
+
+        # run inference through the network
+        # and gather predictions from output layers
+        outs = net.forward(self._get_output_layers(net))
+
+        # initialization
+        class_ids = []
+        confidences = []
+        boxes = []
+        conf_threshold = 0.5
+        nms_threshold = 0.4
+
+        # for each detetion from each output layer 
+        # get the confidence, class id, bounding box params
+        # and ignore weak detections (confidence < 0.5)
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.9:
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
+
+        # apply non-max suppression
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+        self.bound_image = self.image
+        # go through the detections remaining
+        # after nms and draw bounding box
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            
+            self._draw_bounding_box(class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+        if display:
+            # display output image    
+            cv2.imshow("object detection", self.bound_image)
+            # wait until any key is pressed
+            cv2.waitKey()
+
+        return [ {'x': int(b[0]), 'y': int(b[1]), 'w': int(b[2]), 'h': int(b[3])} for b in boxes ]
+
+
+    def _get_output_layers(self, net):
+        """
+        Method to get the output layer names in the architecture
+
+        reference code from:
+        https://www.arunponnusamy.com/yolo-object-detection-opencv-python.html
+        """
+        layer_names = net.getLayerNames()
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        return output_layers
+
+    def _draw_bounding_box(self, class_id, confidence, x, y, x_plus_w, y_plus_h):
+        """
+        Method to draw bounding box on the detected object with class name
+
+        reference code from:
+        https://www.arunponnusamy.com/yolo-object-detection-opencv-python.html
+        """
+        label = str(self.classes[class_id])
+        color = self.COLORS[class_id]
+        cv2.rectangle(self.bound_image, (x,y), (x_plus_w,y_plus_h), color, 2)
+        cv2.putText(self.bound_image, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    def get_person_images(self, display=False):
+        """ 
+        returns array images and their annotations cropped to the persons
+        detected in self.image
+        """
+        boxes = self.get_person_bboxes()
+        person_images = []
+        for b in boxes:
+            img = self.image[b['y']:b['y']+b['h'], b['x']:b['x']+b['w']]
+            annt = self.annotation[b['y']:b['y']+b['h'], b['x']:b['x']+b['w']]
+            person_images.append({'image': img, 'annotation': annt})
+            if display:
+                cv2.namedWindow("person", 0)
+                cv2.imshow("person", img)
+                cv2.waitKey()
+        return person_images
+
+
+if __name__ == '__main__':
+    ip = ImageProcessed(cv2.imread("./clean_data/image/0002.jpg"),
+            cv2.imread('./clean_data/annotation/0002.jpg'))
+    ip.get_person_images(display = True)
 
