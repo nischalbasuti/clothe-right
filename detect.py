68c33ba9f3aaff78208cb966724d446e8bb43c47
nischal basuti <nischalbasuti1@gmail.com>
2019-02-22 20:38:26 +0700

    clean up

--- detect.py
+++ detect.py
@@ -10,35 +10,60 @@ import image_processor as ip
 
 import copy
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--image", help="path to image.")
-parser.add_argument("--model", help="path to trained model h5 file.")
-parser.add_argument("--shape", help="shape of input")
-args = parser.parse_args()
+def generate_heatmap_image(image, annotation):
+    heat_map = copy.deepcopy(image)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            heat_map[i,j, 1] = annotation[i, j] * 255
+    return heat_map
 
-image_source = args.image
-model_source = args.model
-shape = int(args.shape)
+def detect(image_path, model_path):
+    image_path = args.image
+    model_path = args.model
+    shape = int(args.shape)
 
-img = cv2.imread(image_source)
+    imgp = ip.ImageProcessed(cv2.imread(image_path))
+    img = imgp.get_person_images()[0]["image"]
 
-imgLab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    imgLab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-input_img = np.array([ cv2.resize( imgLab, (shape, shape) ) ]) # input to nn should be (1, 32, 32, 3)
+    input_img = np.array([ cv2.resize( imgLab, (shape, shape) ) ]) # input to nn should be (1, 32, 32, 3)
 
-cnn = pcnn.Patched_CNN()
-cnn.load_model(model_source)
-print("...initialized model...")
+    cnn = pcnn.Patched_CNN()
+    cnn.load_model(model_path)
+    print("...initialized model...")
 
-pred = cnn.predict(input_img)
+    pred = cnn.predict(input_img)
 
-annotation_pred = cv2.resize( pred.reshape(shape, shape), (img.shape[1], img.shape[0]) )
+    annotation_pred = cv2.resize( pred.reshape(shape, shape), (img.shape[1], img.shape[0]) )
 
-imgp = ip.ImageProcessed(img, annotation_pred)
-imgp._segment()
+    imgp.image = img
+    imgp.annotation = annotation_pred
 
-imgp.display_segmented_annotation()
-imgp.display_segmented_image()
+    imgp._segment()
 
-cv2.waitKey(0)
+    imgp.display_segmented_annotation()
+    imgp.display_segmented_image()
+
+    return imgp
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", "-i", help="path to image.")
+    parser.add_argument("--model", "-m", help="path to trained model h5 file.")
+    parser.add_argument("--shape", "-s", help="shape of input")
+    args = parser.parse_args()
+
+    imgp_detected = detect(args.image, args.model)
+
+    imgp_detected.display_image()
+    imgp_detected.display_annotation()
+    imgp_detected.display_segmented_annotation()
+    imgp_detected.display_segmented_image()
+
+    heat_map = generate_heatmap_image(imgp_detected.image, imgp_detected.annotation)
+    cv2.namedWindow("heat map", 0)
+    cv2.imshow("heat map", heat_map)
+
+    cv2.waitKey(0)
 
