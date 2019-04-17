c9a094a2d9e6fc84014f10001d250b30586d2189
nischal basuti <nischalbasuti1@gmail.com>
2019-04-18 06:40:35 +0700

    Added error handling for files that are not found.

--- make_dataset.py
+++ make_dataset.py
@@ -26,28 +26,32 @@ def process_mat_files(image_dir_path, annotation_dir_path,
     for image_path in image_paths:
         # open image
         print(image_path)
-        image = cv2.imread(image_path) 
+        try:
+            image = cv2.imread(image_path) 
 
-        # open annotation file
-        image_file_name = os.path.basename(image_path)
-        annotation_file_name = image_file_name[:-3] + "mat"
-        annotation_path = os.path.join(annotation_dir_path, annotation_file_name)
-        print(annotation_path)
+            # open annotation file
+            image_file_name = os.path.basename(image_path)
+            annotation_file_name = image_file_name[:-3] + "mat"
+            annotation_path = os.path.join(annotation_dir_path, annotation_file_name)
+            print(annotation_path)
 
-        annotation = sio.loadmat(annotation_path)['groundtruth']
+            annotation = sio.loadmat(annotation_path)['groundtruth']
 
-        # Get annotation of only tops.
-        tops_annotation = np.zeros(annotation.shape)
-        print(tops_dict.values())
-        for i in range(annotation.shape[0]):
-            for j in range(annotation.shape[1]):
-                tops_annotation[i,j] = 255 if annotation[i,j] in tops_dict.values() else 0
+            # Get annotation of only tops.
+            tops_annotation = np.zeros(annotation.shape)
+            print(tops_dict.values())
+            for i in range(annotation.shape[0]):
+                for j in range(annotation.shape[1]):
+                    tops_annotation[i,j] = 255 if annotation[i,j] in tops_dict.values() else 0
 
-        # Save image and annotation.
-        image_save_path = os.path.join(image_save_dir, image_file_name)
-        annotation_save_path = os.path.join(annotation_save_dir, image_file_name)
-        cv2.imwrite(image_save_path, image)
-        cv2.imwrite(annotation_save_path, tops_annotation)
+            # Save image and annotation.
+            image_save_path = os.path.join(image_save_dir, image_file_name)
+            annotation_save_path = os.path.join(annotation_save_dir, image_file_name)
+            cv2.imwrite(image_save_path, image)
+            cv2.imwrite(annotation_save_path, tops_annotation)
+        except FileNotFoundError:
+            print("file %s not found." % image_path)
+
 
 def process_clean_files(image_dir_path="./clean_data/image",
         annotation_dir_path="./clean_data/annotation",
@@ -92,10 +96,10 @@ def process_clean_files(image_dir_path="./clean_data/image",
             break 
 
 if __name__  == '__main__':
-    # process the clothing-co-parsing dataset.
-    process_mat_files(image_dir_path="./clothing-co-parsing/photos",
-            annotation_dir_path="./clothing-co-parsing/annotations/pixel-level",
-            save_dir='./clean_data')
+    # # process the clothing-co-parsing dataset.
+    # process_mat_files(image_dir_path="./clothing-co-parsing/photos",
+    #         annotation_dir_path="./clothing-co-parsing/annotations/pixel-level",
+    #         save_dir='./clean_data')
 
     # process the cleaned data and save cropped images.
     process_clean_files(image_dir_path="./clean_data/image",
